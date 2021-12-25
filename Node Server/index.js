72597099ad7d4e8df605d0f49824e0be8d09ccd6
Bhaskar Rai <bhaskar9893@gmail.com>
2021-12-25 15:33:40 +0530

    all events for send receive and leave user are done

--- Node Server/index.js
+++ Node Server/index.js
@@ -1,26 +1,30 @@
-
-
 //Server Which will handle socket io connections--
 
-const io = require("socket.io")(8000)
+const io = require("socket.io")(8000);
 
 const users = {};
 //For every New Connection that function Listen--
-io.on("connection",socket=>{
+io.on("connection", (socket) => {
+  //When New User Joined the chat----
 
-    //When New User Joined the chat----
+  socket.on("new-user-joined", (name) => {
+    // console.log("New User",name)
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
 
-    socket.on("new-user-joined", name =>{
-        // console.log("New User",name)
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name);
+  //When Some one send some message--------
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
     });
+  });
+  //When User Leave from the room--
 
-    //When Some one send some message--------
-
-    socket.on("send",message=>{
-        socket.broadcast.emit('receive',{message:message, name:users[socket.id]})
-    });
-
-
-})
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
